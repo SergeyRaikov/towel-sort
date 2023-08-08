c9133bcf431b63ba3ad3633f9c018d3785db4eed
Sergey <rajkovsergej81@gmail.com>
2023-08-08 15:55:30 +0300

    refactor: corrected the code for compliance with the requirements of the tests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  let result = [];
-    for (let i = 0; i < matrix.length; i++) {
-        
-        if (i % 2 !== 0) {
-            matrix[i].reverse()
-        }
-      
+module.exports = function showArray(matrix) {
+  if (matrix !== [] && matrix !== undefined) {
+    for (let i=0; i<matrix.length; i++) {
+        if (i % 2 !== 0) matrix[i].reverse();
     }
-    
-  result = matrix.flat(Infinity)
-  
-    return result;
+    return matrix.flat(Infinity)
+  }
+return [];
 }
